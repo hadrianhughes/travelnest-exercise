2748fac7f2636f3810b9aacbdf48cd48bb78f482
Hadrian Hughes <hadrianhughes@gmail.com>
2023-05-01 16:08:36 +0100

    Get Airbnb property ID from the command line arguments

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 type PropertyInfo = {
   name: string;
   type: string;
@@ -6,4 +8,24 @@ type PropertyInfo = {
   amenities: string[];
 };
 
-console.log('ran');
+const scrape = async () => {
+  try {
+    const response = await axios.get('https://www.airbnb.co.uk/rooms/50633275');
+    console.log(response);
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+const getAirbnbID = (): string => {
+  const flagIdx = process.argv.indexOf('--id');
+  const id = process.argv[flagIdx + 1];
+
+  if (flagIdx === -1 || !id) {
+    throw new Error('You must provide an Airbnb property ID.');
+  }
+
+  return id;
+};
+
+console.log(getAirbnbID());
