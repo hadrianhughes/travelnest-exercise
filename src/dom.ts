da80ec607aca1dd1a1aa0fabd8315c57b098681d
Hadrian Hughes <hadrianhughes@gmail.com>
2023-05-01 18:30:24 +0100

    Get number of bedrooms

--- src/dom.ts
+++ src/dom.ts
@@ -11,6 +11,18 @@ const getType = async (node: ElementHandle): Promise<string | null> => {
   return text;
 };
 
+const getBedrooms = async (node: ElementHandle): Promise<number | null> => {
+  const detailsNode =  await node.waitForSelector('ol');
+  const detailsText = await detailsNode?.evaluate(el => el.textContent);
+
+  const match = detailsText.match(/(\d+) bedrooms?/);
+  if (!match) {
+    return null;
+  }
+
+  return parseInt(match[1]);
+};
+
 export const getPropertyInfo = async (id: string) => {
   const browser = await puppeteer.launch({ headless: 'new' });
   const page = await browser.newPage();
@@ -28,7 +40,7 @@ export const getPropertyInfo = async (id: string) => {
     throw new Error(`could not find complete details for property: ${id}`);
   }
 
-  const [name, type] = await Promise.all([getName(nameNode), getType(overviewNode)]);
+  const [name, type, bedrooms] = await Promise.all([getName(nameNode), getType(overviewNode), getBedrooms(overviewNode)]);
 
-  console.log(name, type);
+  console.log(name, type, bedrooms);
 };
